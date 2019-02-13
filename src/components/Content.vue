466c2b242b6845fd6df4c069468eba38e5ae14bb
Ronnie Ho <ronnieyho@gmail.com>
2019-02-12 20:43:04 -0800

    added remaining child components

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -5,24 +5,31 @@
       <li v-for="child in childComponents">{{ child }}</li>
     </ul>
     {{ weather_data }}
+    <temp-var-chart :tempVar="tempVar"></temp-var-chart>
+    <today-highlights :highlights="highlights"></today-highlights>
   </div>
 </template>
 
 <script>
+import TempVarChart from "./TempVarChart.vue";
+import Highlights from "./Highlights.vue";
+
 export default {
   props: ["weather_data"],
-  data () {
+  components: {
+    "temp-var-chart": TempVarChart,
+    "today-highlights": Highlights
+  },
+  data() {
     return {
-      childComponents: ['TempVarChart.vue', 'Highlights.vue']
-    }
+      childComponents: ["TempVarChart.vue", "Highlights.vue"],
+      tempVar: this.weather_data.temperature,
+      highlights: this.weather_data.highlights
+    };
   },
-  methods: {
-
-  },
-  computed: {
-
-  },
-}
+  methods: {},
+  computed: {}
+};
 </script>
 
 <style>
---
+++ src/components/Visibility.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="visibility">
+    <p>Visibility: {{ visibility }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["highlights"],
+  data() {
+    return {
+      visibility: this.highlights.visibility
+    };
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style>
+</style>
